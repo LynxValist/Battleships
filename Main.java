2f4bb5bf7125328d9fd11bbe0d4fe0d726032d81
LynxValist <78996700+LynxValist@users.noreply.github.com>
2021-10-02 10:20:37 -0700

    main

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    // roww p n gerald t
+    // batolship
+    battleField game1 = new battleField();
+    game1.displayBoard();
+    scan.nextInt();
+  }
+
+}
